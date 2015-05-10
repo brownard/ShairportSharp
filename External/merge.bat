aded1ff7653c5ab3301de04de278307149cda1b0
brownard <brownard@live.co.uk>
2015-05-10 14:01:25 +0100

    Update ILMerge and merge script

--- External/merge.bat
+++ External/merge.bat
@@ -1,7 +1,7 @@
 @echo off
 
 md tmp
-ilmerge /out:tmp\AirPlayer.dll AirPlayer.dll AirPlayer.Common.dll ShairportSharp.dll DirectShowWrapper.dll ZeroconfService.dll BouncyCastle.Crypto.dll
+ilmerge /lib:"C:\Windows\Microsoft.NET\Framework\v4.0.30319" /out:tmp\AirPlayer.dll AirPlayer.dll AirPlayer.Common.dll ShairportSharp.dll DirectShowWrapper.dll ZeroconfService.dll BouncyCastle.Crypto.dll Arm7.dll
 IF EXIST AirPlayer_UNMERGED.dll del AirPlayer_UNMERGED.dll
 ren AirPlayer.dll AirPlayer_UNMERGED.dll
 IF EXIST AirPlayer_UNMERGED.pdb del AirPlayer_UNMERGED.pdb

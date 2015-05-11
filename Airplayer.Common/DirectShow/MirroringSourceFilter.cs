f744abd3cfa1de66c8c2d284b577d93ec02b2a76
brownard <brownard@live.co.uk>
2015-05-11 22:38:52 +0100

    Move AVC media type building to demux track

--- Airplayer.Common/DirectShow/MirroringSourceFilter.cs
+++ Airplayer.Common/DirectShow/MirroringSourceFilter.cs
@@ -23,8 +23,6 @@ namespace AirPlayer.Common.DirectShow
 
     public class MirroringFileParser : FileParser
     {
-        //const int FOURCC_AVC1 = 0x31435641;
-        //static readonly Guid SUBTYPE_AVC1 = new Guid(0x31435641, 0x0000, 0x0010, 0x80, 0x00, 0x00, 0xaa, 0x00, 0x38, 0x9b, 0x71);
         protected MirroringStream stream;
 
         public MirroringFileParser() : base(false) { }
@@ -71,66 +69,6 @@ namespace AirPlayer.Common.DirectShow
         //        m_Tracks[0].AddToCache(ref pd);
         //    }
         //    return S_OK;
-        //}
-
-        //AMMediaType getMediaType()
-        //{
-        //    H264CodecData codecData = stream.CodecData;
-        //    Mpeg2VideoInfo vi = new Mpeg2VideoInfo();
-        //    vi.hdr.SrcRect.right = 432;
-        //    vi.hdr.SrcRect.bottom = 648;
-        //    vi.hdr.TargetRect.right = 432;
-        //    vi.hdr.TargetRect.bottom = 648;
-        //    vi.hdr.AvgTimePerFrame = 400000;
-        //    vi.hdr.BmiHeader.Width = 432;
-        //    vi.hdr.BmiHeader.Height = 648;
-        //    vi.hdr.BmiHeader.Planes = 1;
-        //    vi.hdr.BmiHeader.Compression = FOURCC_AVC1;
-
-        //    vi.dwProfile = (uint)codecData.Profile;
-        //    vi.dwLevel = (uint)codecData.Level;
-        //    vi.dwFlags = (uint)codecData.NALSizeMinusOne + 1;
-
-        //    int offset = 0;
-        //    byte[] sp = new byte[codecData.SPSLength + codecData.PPSLength + 4];
-        //    sp[offset++] = (byte)(codecData.SPSLength >> 8);
-        //    sp[offset++] = (byte)codecData.SPSLength;
-        //    for (int i = 0; i < codecData.SPSLength; i++)
-        //        sp[offset++] = codecData.SPS[i];
-        //    sp[offset++] = (byte)(codecData.PPSLength >> 8);
-        //    sp[offset++] = (byte)(codecData.PPSLength);
-        //    for (int i = 0; i < codecData.PPSLength; i++)
-        //        sp[offset++] = codecData.PPS[i];
-
-        //    vi.cbSequenceHeader = (uint)sp.Length;
-
-        //    AMMediaType amt = new AMMediaType();
-        //    amt.majorType = MediaType.Video;
-        //    amt.subType = SUBTYPE_AVC1;
-        //    amt.temporalCompression = true;
-        //    amt.fixedSizeSamples = false;
-        //    amt.sampleSize = 1;
-        //    setFormat(vi, sp, amt);
-        //    return amt;
-        //}
-
-        //void setFormat(Mpeg2VideoInfo vi, byte[] sp, AMMediaType amt)
-        //{
-        //    int cb = Marshal.SizeOf(vi);
-        //    int add = sp == null || sp.Length < 4 ? 0 : sp.Length - 4;
-        //    IntPtr _ptr = Marshal.AllocCoTaskMem(cb + add);
-        //    try
-        //    {
-        //        Marshal.StructureToPtr(vi, _ptr, true);
-        //        if (sp != null)
-        //            Marshal.Copy(sp, 0, _ptr + cb - 4, sp.Length);
-        //        amt.SetFormat(_ptr, cb + add);
-        //        amt.formatType = FormatType.Mpeg2Video;
-        //    }
-        //    finally
-        //    {
-        //        Marshal.FreeCoTaskMem(_ptr);
-        //    }
         //}
     }
 
@@ -263,5 +201,66 @@ namespace AirPlayer.Common.DirectShow
         {
             return y == 0 ? x : HCF(y, x % y);
         }
+
+        //const int FOURCC_AVC1 = 0x31435641;
+        //static readonly Guid SUBTYPE_AVC1 = new Guid(0x31435641, 0x0000, 0x0010, 0x80, 0x00, 0x00, 0xaa, 0x00, 0x38, 0x9b, 0x71);
+        //AMMediaType getMediaType(H264CodecData codecData)
+        //{
+        //    Mpeg2VideoInfo vi = new Mpeg2VideoInfo();
+        //    vi.hdr.SrcRect.right = 432;
+        //    vi.hdr.SrcRect.bottom = 648;
+        //    vi.hdr.TargetRect.right = 432;
+        //    vi.hdr.TargetRect.bottom = 648;
+        //    vi.hdr.AvgTimePerFrame = 400000;
+        //    vi.hdr.BmiHeader.Width = 432;
+        //    vi.hdr.BmiHeader.Height = 648;
+        //    vi.hdr.BmiHeader.Planes = 1;
+        //    vi.hdr.BmiHeader.Compression = FOURCC_AVC1;
+
+        //    vi.dwProfile = (uint)codecData.Profile;
+        //    vi.dwLevel = (uint)codecData.Level;
+        //    vi.dwFlags = (uint)codecData.NALSizeMinusOne + 1;
+
+        //    int offset = 0;
+        //    byte[] sp = new byte[codecData.SPSLength + codecData.PPSLength + 4];
+        //    sp[offset++] = (byte)(codecData.SPSLength >> 8);
+        //    sp[offset++] = (byte)codecData.SPSLength;
+        //    for (int i = 0; i < codecData.SPSLength; i++)
+        //        sp[offset++] = codecData.SPS[i];
+        //    sp[offset++] = (byte)(codecData.PPSLength >> 8);
+        //    sp[offset++] = (byte)(codecData.PPSLength);
+        //    for (int i = 0; i < codecData.PPSLength; i++)
+        //        sp[offset++] = codecData.PPS[i];
+
+        //    vi.cbSequenceHeader = (uint)sp.Length;
+
+        //    AMMediaType amt = new AMMediaType();
+        //    amt.majorType = MediaType.Video;
+        //    amt.subType = SUBTYPE_AVC1;
+        //    amt.temporalCompression = true;
+        //    amt.fixedSizeSamples = false;
+        //    amt.sampleSize = 1;
+        //    setFormat(vi, sp, amt);
+        //    return amt;
+        //}
+
+        //void setFormat(Mpeg2VideoInfo vi, byte[] sp, AMMediaType amt)
+        //{
+        //    int cb = Marshal.SizeOf(vi);
+        //    int add = sp == null || sp.Length < 4 ? 0 : sp.Length - 4;
+        //    IntPtr _ptr = Marshal.AllocCoTaskMem(cb + add);
+        //    try
+        //    {
+        //        Marshal.StructureToPtr(vi, _ptr, true);
+        //        if (sp != null)
+        //            Marshal.Copy(sp, 0, _ptr + cb - 4, sp.Length);
+        //        amt.SetFormat(_ptr, cb + add);
+        //        amt.formatType = FormatType.Mpeg2Video;
+        //    }
+        //    finally
+        //    {
+        //        Marshal.FreeCoTaskMem(_ptr);
+        //    }
+        //}
     }
 }
